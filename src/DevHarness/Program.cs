099c06f28ac909acedb03f0561321cf6acdd728a
Michael Roth <mtroth@outlook.com>
2017-06-07 18:04:16 -0700

    Added support for $expand($compute)

--- src/DevHarness/Program.cs
+++ src/DevHarness/Program.cs
@@ -13,19 +13,29 @@ namespace DevHarness
         static void Main(string[] args)
         {
             Uri root = new Uri("http://server/");
-            Uri url = new Uri("/Entities?$compute=cast(Prop1, 'Edm.String') as Property1AsString, tolower(Prop1) as Property1Lower", UriKind.Relative);
+            Uri url = new Uri("/Entities?$compute=cast(Prop1, 'Edm.String') as Property1AsString, tolower(Prop1) as Property1Lower&$expand=Nav1($compute=cast(Prop1, 'Edm.String') as NavProperty1AsString)", UriKind.Relative);
 
             EdmModel model = new EdmModel();
             EdmEntityType elementType = model.AddEntityType("DevHarness", "Entity");
+            EdmEntityType targetType = model.AddEntityType("DevHarness", "Navigation");
             
             EdmTypeReference typeReference = new EdmStringTypeReference(EdmCoreModel.Instance.GetPrimitiveType(EdmPrimitiveTypeKind.String), false);
-            EdmProperty property = new EdmStructuralProperty(elementType, "Prop1", typeReference);
-            elementType.AddProperty(property);
+            elementType.AddProperty(new EdmStructuralProperty(elementType, "Prop1", typeReference));
+            targetType.AddProperty(new EdmStructuralProperty(targetType, "Prop1", typeReference));
+
+            EdmNavigationPropertyInfo propertyInfo = new EdmNavigationPropertyInfo();
+            propertyInfo.Name = "Nav1";
+            propertyInfo.Target = targetType;
+            propertyInfo.TargetMultiplicity = EdmMultiplicity.One;
+            EdmProperty navigation = EdmNavigationProperty.CreateNavigationProperty(elementType, propertyInfo);
+            elementType.AddProperty(navigation);
+
             EdmEntityContainer container = model.AddEntityContainer("Default", "Container");
             container.AddEntitySet("Entities", elementType);
 
             ODataUriParser parser = new ODataUriParser(model, root, url);
             ComputeClause clause = parser.ParseCompute();
+            SelectExpandClause se = parser.ParseSelectAndExpand();
         }
     }
 }
