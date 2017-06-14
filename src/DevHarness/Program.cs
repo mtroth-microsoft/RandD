1981554f1d9dea160a0481577927b1ae04ae50da
mtroth-microsoft <mtroth@microsoft.com>
2017-06-13 18:59:43 -0700

    Switch to primitive type casting.

--- src/DevHarness/Program.cs
+++ src/DevHarness/Program.cs
@@ -13,6 +13,33 @@ namespace DevHarness
         static void Main(string[] args)
         {
             Uri root = new Uri("http://server/");
+            Uri url = new Uri("http://server/Entities?$select=Prop1/Edm.String&$filter=Prop2/Edm.String eq 'sdb'&$orderby=Prop2/Edm.String");
+            url = new Uri("http://server/Entities?$filter=cast(Prop2, Edm.String) eq 'sdb'");
+
+            EdmModel model = new EdmModel();
+            EdmEntityType elementType = model.AddEntityType("DevHarness", "Entity");
+            EdmComplexType complexType = model.AddComplexType("DevHarness", "Complex", null, true);
+            EdmComplexType derivedType = model.AddComplexType("DevHarness", "Derived", complexType, false);
+
+            EdmComplexTypeReference complexTypeReference = new EdmComplexTypeReference(complexType, false);
+            EdmTypeReference typeReference = new EdmStringTypeReference(EdmCoreModel.Instance.GetPrimitiveType(EdmPrimitiveTypeKind.String), false);
+            elementType.AddProperty(new EdmStructuralProperty(elementType, "Prop1", typeReference));
+            elementType.AddProperty(new EdmStructuralProperty(elementType, "Prop2", typeReference));
+            elementType.AddProperty(new EdmStructuralProperty(elementType, "Prop3", complexTypeReference));
+            derivedType.AddProperty(new EdmStructuralProperty(derivedType, "Prop4", typeReference));
+
+            EdmEntityContainer container = model.AddEntityContainer("Default", "Container");
+            container.AddEntitySet("Entities", elementType);
+
+            ODataUriParser parser = new ODataUriParser(model, root, url);
+            //OrderByClause orderby = parser.ParseOrderBy();
+            //SelectExpandClause select = parser.ParseSelectAndExpand();
+            FilterClause filter = parser.ParseFilter();
+        }
+
+        private static void TestCompute()
+        {
+            Uri root = new Uri("http://server/");
             Uri url = new Uri("/Entities?$compute=cast(Prop1, 'Edm.String') as Property1AsString, tolower(Prop1) as Property1Lower&$expand=Nav1($compute=cast(Prop1, 'Edm.String') as NavProperty1AsString;$expand=SubNav1($compute=cast(Prop1, 'Edm.String') as SubNavProperty1AsString))", UriKind.Relative);
 
             EdmModel model = new EdmModel();
