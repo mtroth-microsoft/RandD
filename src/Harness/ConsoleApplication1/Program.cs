26cc29a34c9e98e8cef400ebd2fe746b459579c0
Michael Roth <mtroth@outlook.com>
2023-08-12 09:09:49 -0700

    Add state machine

--- src/Harness/ConsoleApplication1/Program.cs
+++ src/Harness/ConsoleApplication1/Program.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using Infrastructure.Parser.UriParser;
-
+﻿
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<ExpressionToken> tokens = new List<ExpressionToken>();
-            ExpressionLexer lexer = new ExpressionLexer("(foo eq true) or ('yes' eq tolower(bar))", false, false);
-            while (true)
-            {
-                ExpressionToken token = lexer.NextToken();
-                if (token.Kind == ExpressionTokenKind.End)
-                {
-                    break;
-                }
+            Juliet.Program.Run();
 
-                tokens.Add(token);
-            }
+            //List<ExpressionToken> tokens = new List<ExpressionToken>();
+            //ExpressionLexer lexer = new ExpressionLexer("(foo eq true) or ('yes' eq tolower(bar))", false, false);
+            //while (true)
+            //{
+            //    ExpressionToken token = lexer.NextToken();
+            //    if (token.Kind == ExpressionTokenKind.End)
+            //    {
+            //        break;
+            //    }
 
-            Uri url = new Uri("http://server/service/Entities?$filter=(foo eq true) or ('yes' eq tolower(bar)) or foobars/any()&$orderby=bar desc,foo&$top=1&$compute=bar as computedBar");
-            List <CustomQueryOptionToken> options = QueryOptionUtils.ParseQueryOptions(url);
+            //    tokens.Add(token);
+            //}
 
-            UriQueryExpressionParser parser = new UriQueryExpressionParser(ODataUriParserSettings.DefaultFilterLimit);
-            QueryToken qt = parser.ParseFilter("(foo eq true) or ('yes' eq tolower(bar))");
+            //Uri url = new Uri("http://server/service/Entities?$filter=(foo eq true) or ('yes' eq tolower(bar)) or foobars/any()&$orderby=bar desc,foo&$top=1&$compute=bar as computedBar");
+            //List <CustomQueryOptionToken> options = QueryOptionUtils.ParseQueryOptions(url);
+
+            //UriQueryExpressionParser parser = new UriQueryExpressionParser(ODataUriParserSettings.DefaultFilterLimit);
+            //QueryToken qt = parser.ParseFilter("(foo eq true) or ('yes' eq tolower(bar))");
 
             return;
         }
