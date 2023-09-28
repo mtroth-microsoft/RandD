3c2ebc98ddda3e53763c8d53d689cc2bd4b929b9
Michael Roth <mtroth@outlook.com>
2023-09-28 14:28:07 -0700

    add sql inserts for relevant post season game types

--- src/MlbDataPump/Model/GameType.cs
+++ src/MlbDataPump/Model/GameType.cs
@@ -1,4 +1,9 @@
-﻿
+﻿/*
+  insert dbo.GameTypeMap select '2023-10-03 00:00:00.000', '2023-10-05 00:00:00.000', 'WildCard', 70, 'F'
+  insert dbo.GameTypeMap select '2023-10-07 00:00:00.000', '2023-10-14 00:00:00.000', 'DivisionalPlayoff', 68, 'D'
+  insert dbo.GameTypeMap select '2023-10-15 00:00:00.000', '2023-10-24 00:00:00.000', 'LeaguePlayOff', 76, 'L'
+  insert dbo.GameTypeMap select '2023-10-27 00:00:00.000', '2023-11-04 00:00:00.000', 'WorldSeries', 87, 'W'
+*/
 namespace MlbDataPump.Model
 {
     public enum GameType
